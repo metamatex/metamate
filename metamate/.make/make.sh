f0f5d3d2535e84d43149edb12c19cf3865669570
Philipp Wördehoff <ph.woerdehoff@gmail.com>
2020-03-09 08:57:53 +0100

    .

--- metamate/.make/make.sh
+++ metamate/.make/make.sh
@@ -14,5 +14,32 @@ function chore {
 }
 
 function release {
-    goreleaser release --rm-dist -f make/.goreleaser.yml
+    docker run -i --rm \
+        -v $(pwd)/..:/go/src/github.com/metamatex/metamatemmono \
+        -w /go/src/github.com/metamatex/metamatemmono/metamate \
+        -e "GOOS=linux" \
+        -e "GOARCH=amd64" \
+        -e "CGO_ENABLED=1" \
+        golang \
+        go build -o dist/metamate cmd/metamate/main.go
+
+    TAG=$(git describe --exact-match --tags $(git log -n1 --pretty='%h'))
+    REV=$(git rev-parse HEAD)
+    DATE=$(date "+%Y-%m-%d")
+    echo $TAG
+    docker build \
+        --pull \
+        --file .make/Dockerfile \
+        --label=org.opencontainers.image.created=$DATE \
+        --label=org.opencontainers.image.name=metamate \
+        --label=org.opencontainers.image.revision=$REV \
+        --label=org.opencontainers.image.version=$TAG \
+        --label=org.opencontainers.image.source=https://github.com/metamatex/metamatemono \
+        --label=repository=http://github.com/metamatex/metamatemono \
+        --label=homepage=http://metamate.io \
+        --tag metamatex/metamate:latest \
+        --tag metamatex/metamate:$TAG \
+        .
+
+    # docker push metamatex/metamate:latest
 }
