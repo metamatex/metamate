1637aac5ae6b208472bb6c71818fe6abc1f3093c
Philipp Wördehoff <ph.woerdehoff@gmail.com>
2020-05-04 14:17:48 +0200

    added fake tag to test-release task

--- metamate/.make/make.sh
+++ metamate/.make/make.sh
@@ -22,11 +22,41 @@ function chore {
     golint ./...
 }
 
-function build_image {
+function release {
     TAG=$(git describe --exact-match --tags $(git log -n1 --pretty='%h'))
-        REV=$(git rev-parse HEAD)
-        DATE=$(date "+%Y-%m-%d")
-        VERSION=${TAG//v}
+            REV=$(git rev-parse HEAD)
+            DATE=$(date "+%Y-%m-%d")
+            VERSION=${TAG//v}
+
+        docker run -i --rm \
+            -v $(pwd)/..:/go/src/github.com/metamatex/metamate \
+            -w /go/src/github.com/metamatex/metamate/metamate \
+            -e "GOOS=linux" \
+            -e "GOARCH=amd64" \
+            -e "CGO_ENABLED=1" \
+            golang \
+            go build -ldflags="-X 'main.version=${VERSION}' -X 'main.date=${DATE}' -X 'main.commit=${REV}'" -o dist/metamate cmd/metamate/main.go
+
+        docker build \
+            --pull \
+            --file .make/Dockerfile \
+            --label=org.opencontainers.image.created=$DATE \
+            --label=org.opencontainers.image.name=metamate \
+            --label=org.opencontainers.image.revision=$REV \
+            --label=org.opencontainers.image.version=$TAG \
+            --label=org.opencontainers.image.source=https://github.com/metamatex/metamate \
+            --label=repository=http://github.com/metamatex/metamate \
+            --label=homepage=http://metamate.io \
+            --tag metamatex/metamate:latest \
+            --tag metamatex/metamate:$TAG \
+            .
+
+    docker push metamatex/metamate:latest
+    docker push metamatex/metamate:$TAG
+}
+
+function test_release {
+    TAG=v0.0.0
 
     docker run -i --rm \
         -v $(pwd)/..:/go/src/github.com/metamatex/metamate \
@@ -50,17 +80,6 @@ function build_image {
         --tag metamatex/metamate:latest \
         --tag metamatex/metamate:$TAG \
         .
-}
-
-function release {
-    build_image
-
-    docker push metamatex/metamate:latest
-    docker push metamatex/metamate:$TAG
-}
-
-function test_release {
-    build_image
 }
 
 function deploy {
