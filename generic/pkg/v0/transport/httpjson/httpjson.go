16b7270e6fecdb1fb6da9db015b0770e04ef4cfd
Philipp Wördehoff <ph.woerdehoff@gmail.com>
2020-05-16 09:45:55 +0200

    removed transport folder

--- generic/pkg/v0/transport/httpjson/httpjson.go
+++
@@ -1,8 +0,0 @@
-package httpjson
-
-const (
-	METAMATE_TYPE_HEADER = "X-MetaMate-Type"
-	CONTENT_TYPE_JSON = "application/json; charset=utf-8"
-	CONTENT_TYPE_HEADER = "Content-type"
-	AUTHORIZATION_HEADER = "Authorization"
-)
